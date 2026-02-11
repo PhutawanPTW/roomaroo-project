b95555a8dcdecc73ed9e26bb2d906f986ff8cc66
PtwJIWLIW <161490194+PhutawanPTW@users.noreply.github.com>
2026-02-12 00:07:00 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     appId: '1:782979223053:web:0ed401a937a53890158f44',
     measurementId: 'G-X6MZ8F81G1'
   },
-  backendApiUrl: 'https://dormroomaroo-backend.onrender.com',
+  backendApiUrl: 'https://dormroomaroo-backend.onrender.com/api',
   fastApiUrl: 'https://roomaroo-textclassification.onrender.com',
   mapTilerApiKey: 'Gpwk2Mpi9cl8hUkVrf6f'
 };
